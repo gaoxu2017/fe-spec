b984cea856b87cc6811f7f9833986b58f67682db
gaoxu <Gaoxu13167316938@163.com>
2023-08-28 11:29:49 +0800

    feat: this new feat

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+#确保脚本抛出遇到错误
+set -e
+
+#生成静态资源站点
+push_addr=`git remote https://github.com/gaoxu2017/lintArrray-github.io.git --push origin`
+commit_info=`git descrite --all --always --long`
+dist_path=`docs\.vuepress\dist`
+push_branch=gh-pages
+
+npm run docs:build #生成静态资源站点
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init -A
+git commit -m "deploy"
+
+git push -f $push_addr HEAD:$push_branch
+
+cd-
+#删除静态资源文件
+rm -rf $dist_path
+
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
