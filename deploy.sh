d8a888a395d597ed133f74c5c61d2e69b2c8777f
gaoxu <Gaoxu13167316938@163.com>
2023-08-28 14:36:49 +0800

    feat: deploy modify

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git init
 git add -A
 git commit -m "deploy"
 
-git push -f https://github.com/gaoxu2017/fe-spec.git HEAD:$push_branch
+git push -f https://github.com/gaoxu2017/fe-spec.git main:gh-pages
 
 cd-
 #删除静态资源文件
